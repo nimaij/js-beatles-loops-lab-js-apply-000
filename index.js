0e1c0ca9e8a877b33a0a54b86aa806dc7d189b3e
Jiamin Lim <jiaminlim93@gmail.com>
2018-11-14 16:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   var newFacts=[];
-  for (i = 0; i < facts.length; i++) { 
+  for (var i = 0; i < facts.length; i++) { 
     newFacts.push(`${facts[i]}!!!"`)
   }
   return newFacts;
