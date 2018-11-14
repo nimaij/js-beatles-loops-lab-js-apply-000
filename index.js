09533075b5e2fece203262b465291f7cf6fbe77a
Jiamin Lim <jiaminlim93@gmail.com>
2018-11-14 16:17:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   arr=[]
-  
+  for (i = 0; i < cars.length; i++) { 
+    text += cars[i] + "<br>";
+}
 }
 
 function johnLennonFacts(){
