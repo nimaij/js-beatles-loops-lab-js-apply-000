f1be079b03ac87b48ae494c3594b00d7c8f3a90d
Jiamin Lim <jiaminlim93@gmail.com>
2018-11-14 16:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var allPlayers=[];
-  for (i = 0; i < musicians.length; i++) { 
+  for (var i = 0; i < musicians.length; i++) { 
     allPlayers.push(musicians[i]+" plays "+instruments[i])
   }
   return allPlayers;
